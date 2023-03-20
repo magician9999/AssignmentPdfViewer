c70570e94bfc20fb4115b7f6619d660c51f9309f
magician9999 <90276850+magician9999@users.noreply.github.com>
2023-03-20 19:14:06 +0530

    added dropbox

---
+++ src/DropdownBox.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+
+const DropdownBox = () => {
+
+  const { t } = useTranslation();
+
+  const handleLanguageChange = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+
+
+  return (
+    <>
+     <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+               Choose Language 
+               </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleLanguageChange("fr")}>English(default)</Dropdown.Item>
+              <Dropdown.Item >Hindi</Dropdown.Item>
+              <Dropdown.Item >Tamil</Dropdown.Item>
+              </Dropdown.Menu>
+      </Dropdown>
+      
+    </>
+  );
+};
+
+export default DropdownBox;
