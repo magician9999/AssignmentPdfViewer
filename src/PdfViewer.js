2f855765b1c595a9fe1266a65b2a17fd3de0a518
magician9999 <90276850+magician9999@users.noreply.github.com>
2023-03-20 19:08:09 +0530

    change dropbox component

--- src/PdfViewer.js
+++ src/PdfViewer.js
@@ -1,23 +1,32 @@
 import React,{useState,useEffect} from 'react'
 import {  LocalizationContext, LocalizationMap, Worker ,Viewer } from '@react-pdf-viewer/core';
+
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Dropdown from 'react-bootstrap/Dropdown';
+
 import './PdfViewer.css';
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+import DropdownBox from './DropdownBox';
+
 
 const PdfViewer = () => {
 
 
 
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
+  
 
 
  
   
   const [pdfFile,setpdffile]=useState(null);
   const [viewPdf, setViewPdf]=useState(null);
+
+  const { t } = useTranslation();
   
   //how many file types are allowed 
   const fileType = ['application/pdf']
@@ -61,40 +70,35 @@ const PdfViewer = () => {
        else {
         setViewPdf(null)
           }
-      }
+        }
+      
+
+
+
 
 
     return (
-   
     
     <div className='container'>
-    <h1 className="main-heading"><span>SWIPE</span> PDF Viewer</h1>
+        <h1 className="main-heading"><span>SWIPE</span> PDF Viewer</h1>
         <form onSubmit={handleSubmit}>
             <input type='file' className='form-control' onChange={handleChange}/>
             <div className='dropandsubmit'>
               <button type='submit' className='btn btn-success'>View PDF</button>
-              <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-               Choose Language 
-               </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Hindi</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">English</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Tamil</Dropdown.Item>
-              </Dropdown.Menu>
-               </Dropdown>
+              <DropdownBox/>
            </div>
-
         </form>
+
       <h2>View PDF</h2>
       <div className = "pdf-container">
         
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
             {viewPdf && <> 
-                   <Viewer fileUrl={viewPdf}
+                   <Viewer 
+                   fileUrl={viewPdf}
                   
-                   plugins={[defaultLayoutPluginInstance]}/>
+                   plugins={[defaultLayoutPluginInstance]}
+                   />
                   </>}
                 {!viewPdf && <>No PDF</>}
         </Worker>
